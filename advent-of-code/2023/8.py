38ad3c5a69fe3563da63048acd77f6ab3ed4709a
jlassus <43183168+jlassus@users.noreply.github.com>
2023-12-08 20:49:02 +0200

    Cleanup.

--- advent-of-code/2023/8.py
+++ advent-of-code/2023/8.py
@@ -21,12 +21,6 @@ LR
 XXX = (XXX, XXX)
 """
 
-def parse_network(lines):
-    nodes = {}
-    for line in lines:
-        nodes[line[0:3]] = (line[7:10], line[12:15])
-    return nodes
-
 def steps_until_z(node, network, instructions):
     step = 0
     lr_index = {'L': 0, 'R': 1}
@@ -38,7 +32,7 @@ def steps_until_z(node, network, instructions):
 
 def f(data, part, debug):
     instructions = data[0]
-    network = parse_network(data[2:])
+    network = {line[0:3]: (line[7:10], line[12:15]) for line in data[2:]}
     if part == 1:
         return steps_until_z('AAA', network, instructions)
     return math.lcm(*(steps_until_z(n, network, instructions) for n in network if n[-1] == 'A'))
