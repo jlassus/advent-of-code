ffa9c92d6fbbd1d580123175bff1dcdbbbbde3f3
jlassus <43183168+jlassus@users.noreply.github.com>
2023-12-09 19:28:25 +0200

    Combine into one expression.

--- advent-of-code/2022/4.py
+++ advent-of-code/2022/4.py
@@ -11,8 +11,6 @@ def f(data, part, debug):
     overlap = 0
     for line in data:
         n = tuple(map(int, line.replace(',', '-', 1).split('-')))
-        if part == 1:
-            overlap += ((n[2] >= n[0] and n[3] <= n[1]) or (n[0] >= n[2] and n[1] <= n[3]))
-        else:
-            overlap += (n[2] <= n[1] and n[3] >= n[0])
+        overlap += ((n[2] <= n[1] and n[3] >= n[0])
+                    and (part == 2 or (n[2] >= n[0] and n[3] <= n[1]) or (n[0] >= n[2] and n[1] <= n[3])))
     return overlap
